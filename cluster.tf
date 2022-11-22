79300e41e9eee9f5f5c5d0b0c65e4435b5b5476a
vamsi434 <vamsibusa@gmail.com>
2022-11-22 21:15:43 +0530

    ws

--- cluster.tf
+++ cluster.tf
@@ -3,14 +3,14 @@
 #  engine                  = "docdb"
 #  master_username         = "admin1"
 #  master_password         = "roboshop1"
-# true only during in prod, we will take a snapshot and that time value wiil bw false
+ #true only during in prod, we will take a snapshot and that time value wiil bw false
 #  skip_final_snapshot     = true
+#  db_subnet_group_name = aws_docdb_subnet_group.docdb.name
 #}
 #creates subnet group 
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-mongo-${var.ENV}"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-
   tags = {
     Name = "My docdb subnet group"
   }
