9b4f59ad3289dc41e7f3c40336606ccaf391783e
vamsi434 <vamsibusa@gmail.com>
2022-11-22 20:50:34 +0530

    qsm

--- cluster.tf
+++ cluster.tf
@@ -1,11 +1,11 @@
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.ENV}"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
+#resource "aws_docdb_cluster" "docdb" {
+#  cluster_identifier      = "roboshop-${var.ENV}"
+#  engine                  = "docdb"
+#  master_username         = "admin1"
+3  master_password         = "roboshop1"
 # true only during in prod, we will take a snapshot and that time value wiil bw false
-  skip_final_snapshot     = true
-}
+#  skip_final_snapshot     = true
+#}
 #creates subnet group 
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-mongo-${var.ENV}"
