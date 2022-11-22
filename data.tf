e50db59a6a3f48b9728ce972354ef5e9ea3ec58a
vamsi434 <vamsibusa@gmail.com>
2022-11-22 20:36:43 +0530

    pq

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "terraform_remote_state" "vpc" {
     backend = "s3"
     config = {
-    bucket = "b50-tf-state-bucket"
-    key    = "vpc/${var.ENV}/terraform.tfstate"
-    region = "us-east-1"
+       bucket = "b50-tf-state-bucket"
+       key    = "vpc/${var.ENV}/terraform.tfstate"
+       region = "us-east-1"
 
     }
 }
